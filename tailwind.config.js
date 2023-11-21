7ac59eb9d8ace0485e063a082201deff4d71871d
Mark-Adonteng <144809687+Mark-Adonteng@users.noreply.github.com>
2023-11-21 09:21:17 +0000

    fb login page clone with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin: {
+        '115': '115rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
